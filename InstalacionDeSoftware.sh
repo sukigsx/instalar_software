c79bf2fa8412a4bbc85bb72d66b0e91031a3dd1d
sukigsx <sukigsx.mbsistemas@gmail.com>
2025-08-18 12:12:58 +0200

    a

--- InstalacionDeSoftware.sh
+++ InstalacionDeSoftware.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ruta_ejecucion=$(dirname "$(readlink -f "$0")")
-export version="1s añadido navegadoress"
+export version="1 añadido navegadoress"
 conexion="Sin comprobar"
 software="Sin comprobar"
 actualizado="No se ha podido comprobar la actualizacion del script"
