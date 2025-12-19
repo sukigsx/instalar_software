1e7908ef4383536bfbb423ace07c14be0f854f42
sukigsx <sukigsx.mbsistemas@gmail.com>
2025-12-19 19:51:41 +0100

    Update InstalacionDeSoftware.sh

--- InstalacionDeSoftware.sh
+++ InstalacionDeSoftware.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ruta_ejecucion=$(dirname "$(readlink -f "$0")")
-export version="2.1 Actualizado a base debian13 y nuevo software."
+export version="2.1 Actualizado a base debian13 y nuevo software añadido."
 conexion="Sin comprobar"
 software="Sin comprobar"
 actualizado="No se ha podido comprobar la actualizacion del script"
